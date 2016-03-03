992c7699795f2962677ec3ec03a0fcfca03a5358
recervictory <recervictory@gmail.com>
2016-03-03 10:15:04 +0530

    CountWithPseudocounts.py Finally Done

---
+++ Java/loopy.java
@@ -0,0 +1,25 @@
+/*
+ * -----------------------------------------------------------------
+ * File Name	:loopy.java
+ * DEVELOPER	:Victor Frankenstein (Banerjee)
+ * Email		:<recervictory@gmail.com>
+ * Date			:2016-02-27
+ * 
+ * 
+ * Copyright 2016 Victor Frankenstein (Banerjee) <recervictory@gmail.com>
+ * -----------------------------------------------------------------
+ */
+
+
+public class loopy{
+	public static void main(String[] args){
+		int x=1;
+		System.out.println("Before the Loop");
+		while(x<4){
+			System.out.println("In the loop");
+			System.out.println("Value of x is " +x);
+			x=x+1;
+			}
+		System.out.println("After the loop");
+		}
+	}
---
+++ Python/CountWithPseudocounts.py
@@ -0,0 +1,43 @@
+#  -----------------------------------------------------------------
+#  File Name	:CountWithPseudocounts.py
+#  DEVELOPER	:Victor Frankenstein (Banerjee)
+#  Email		:<recervictory@gmail.com>
+#  Date			:2016-02-28
+#  
+#  
+#  Copyright 2016 Victor Frankenstein (Banerjee) <recervictory@gmail.com>
+#  -----------------------------------------------------------------
+
+
+def CountWithPseudocounts(Motifs):
+	count = {} # initializing the count dictionary
+    # your code here
+	t = len(Motifs)
+	k = len(Motifs[0])
+	for symbol in "GACT":
+		count[symbol]=[]
+		for i in range(k):
+			count[symbol].append(1) #set value as zero 
+	for i in range(t):
+		for j in range(k):
+			symbol = Motifs[i][j]
+			count[symbol][j] += 1
+	return count
+
+def ProfileWithPseudocounts(Motifs):
+	t = len(Motifs)
+	k = len(Motifs[0])
+	profile = {} # output variable
+    # your code here
+	count=CountWithPseudocounts(Motifs)
+	for symbol in "GACT":
+		profile[symbol]=[]
+		for i in range(k):
+			profile[symbol].append(float(float(count[symbol][i])/float(t+4))) #set value as zero
+	return profile
+
+
+ 
+with open('CountWithPseudocounts.txt') as input_data:
+	dna_list = [line.strip() for line in input_data]
+	print(ProfileWithPseudocounts(dna_list))
