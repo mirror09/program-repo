4ae3b047039847ef0d9e25b33ac6300b33fc9122
recervictory <recervictory@gmail.com>
2016-03-23 20:20:28 +0530

    CodeFights Repo Add

---
+++ CodeFights/arrayMinimumAboveBound.py
@@ -0,0 +1,22 @@
+'''
+Problem:
+		Given an array of integers inputArray and an integer bound, 
+		find the smallest array element strictly greater than bound.
+		inputArray = [1, 3, 5, 4, 2, 6] and bound = 3, the output should be 4.
+		
+		https://codefights.com/task/kaAbSQQP3ioXQk3Nk
+'''
+
+
+def arrayMinimumAboveBound(inputArray, bound):
+
+	best = -1
+	for i in range(len(inputArray)):
+		if inputArray[i] > bound and (best == -1 or inputArray[i] < best):
+			best = inputArray[i]
+			print best
+	return best
+
+
+print arrayMinimumAboveBound([1, 3, 5, 4, 2, 6],3)
+print arrayMinimumAboveBound([1, 3, 5, 4, 2, 6],3)
